64b61220736546fe829b8fa8a2646d14800c11a8
Chiyusiguyuan <765451062@qq.com>
2018-10-11 21:09:39 +0800

    这是10月11日更新的代码

--- secoo/index.js
+++ secoo/index.js
@@ -8,83 +8,96 @@ define(['jquery','jquery-cookie'],function($){
                 $('.showServe').css('display','none')
 			});
             $(".showServe").mouseenter(function(){
-                $('.showServe').css('display','block')
+                $(this).css('display','block')
 			});
 			$(".showServe").mouseleave(function(){
-                $('.showServe').css('display','none')
+                $(this).css('display','none')
 			});
-
+            //导航栏图片滚动部分
             $.ajax({
-                url: '../data/data1.json',
-                type: "GET",
-                success: function(res){
-                    var html1 = "";
-                    var html2 = '';
-                    for(var i = 0; i < res.length+1; i++){
-
-                        if(i == res.length){
-                            html1 += `<a href=""><li><img src="${res[0].img}" alt=""></li></a>`;
-                        }else{
-                            html1 += `<a href=""><li><img src="${res[i].img}" alt=""></li></a>`;
-                            html2 += `<li></li>`;
-                        }
-					}
-					$(".containerbody ul").html(html1);
-                    $(".containerbody ol").html(html2);
+                url:'../data/data1.json',
+                type:'GET',
+                success:function(res){
+                    for(var i = 0; i < res.length; i++){
+                        $(`<a href=""><li><img src="${res[i].img}" alt=""></li></a>`).appendTo('.containerbody ul');
+                        $(`<li></li>`).appendTo('.containerbody ol');
+                        $('.containerbody ul a').eq(i).find('li').css('z-index',`${10-i}`);
+                    }
                 },
-                error: function(msg){
+                error :function(msg){
                     alert(msg);
                 }
             })
 
+            var iNow = -1 ;
+            var timer = null;
             var oOl = $('.containerbody').find('ol');
             var oUl = $(".containerbody").find("ul");
-            var iNow = -1;
-            var timer = null;
-            $(".containerbody ul li").eq(0).css('opacity','1');
-            oOl.on('click','oBtns',function(){
+            oOl.on('click','li',function(){
                 iNow = $(this).index();
-
                 tab();
-            })
-            $('.containerbody ol').on('click', 'li', function(){
-                iNow = $(this).index();
-                $(".containerbody ul li").stop().animate({
+                $('.containerbody ul li').stop().animate({
                     opacity : 0
-                },300)
-                $(".containerbody ul li").eq(iNow).stop().animate({
+                },1000)
+                $('.containerbody ul li').eq(iNow).stop().animate({
                     opacity : 1
-                },300)
-                tab();
+                },1000)
             })
-            //添加自动滚动
-            timer = setInterval(timerInner, 4000);
-            $(".containerbody").hover(function(){
+
+            timer = setInterval(timerInner,1000);
+            $('.containerbody').hover(function(){
                 clearInterval(timer);
-            }, function(){
-                timer = setInterval(timerInner, 4000);
+            },function(){
+                timer = setInterval(timerInner,1000);
             })
 
             function timerInner(){
                 iNow++;
                 tab();
             }
-            //点击切换图片
-            function tab(){
-                $('.containerbody ol li').attr("class", "").eq(iNow).attr("class", "active");
-                // $('.containerbody ul li').attr('class','').eq(iNow).attr('class','on');
 
-                $(".containerbody ul li").eq(iNow).stop().animate({
-                    opacity : 1
+            function tab(){
+                $('.containerbody ol li').attr('class','').eq(iNow).attr('class','active');
+                $('.containerbody ul li').eq(iNow).stop().animate({
+                    opacity : 0
                 },1000,function(){
-                    if(iNow == $(".containerbody ul li").size() -1){
-                        $(".containerbody ul li").css('opacity','0');
+                    if(iNow ==  $(".containerbody ul li").size() -1){
                         iNow = -1;
-                        // $(".containerbody ul li").eq(6).css('opacity','1')
+                        $('.containerbody ul li').stop().animate({
+                            opacity : 1
+                        },1000)
                     }
                 })
             }
 
+            $('.containerbody .slider-left').click(function(){
+                $('.containerbody ol li').attr('class','').eq(iNow -1).attr('class','active');
+                $('.containerbody ul li').stop().animate({
+                    opacity : 0
+                },1000)
+                $('.containerbody ul li').eq(iNow -1).stop().animate({
+                    opacity : 1
+                },1000)
+                iNow--;
+                if(iNow == -1){
+                    iNow = 6;
+                }
+            })
+            $('.containerbody .slider-right').click(function(){
+                $('.containerbody ol li').attr('class','').eq(iNow +1).attr('class','active');
+                $('.containerbody ul li').stop().animate({
+                    opacity : 0
+                },1000)
+                $('.containerbody ul li').eq(iNow +1).stop().animate({
+                    opacity : 1
+                },1000)
+                iNow++;
+                if(iNow == 6){
+                    iNow = -1;
+                }
+            })
+            //导航栏图片滚动部分结束
+            //左右边栏导航
             $(".containerbody").mouseenter(function(){
                 $('.slider-left').css('opacity','0.6').css('transition','0.7s')
             });
@@ -97,8 +110,8 @@ define(['jquery','jquery-cookie'],function($){
             $(".containerbody").mouseleave(function(){
                 $('.slider-right').css('opacity','0').css('transition','0.7s')
             });
-
-
+            //左右边栏导航结束
+            //主页图片及文字加载
             $.ajax({
                 url: '../data/data2.json',
                 type: "GET",
@@ -119,7 +132,8 @@ define(['jquery','jquery-cookie'],function($){
                     alert(msg);
                 }
             })
-
+            //主页图片及文字加载结束
+            //主页图片及文字描述的动画效果开始
             $('.activity-list').on('mouseenter', 'a', function(){
                 $(this).css('color','orange').css('transition','0.7s');
                 $(this).find('div').css('opacity','0.7').css('transition','0.7s');
@@ -128,20 +142,15 @@ define(['jquery','jquery-cookie'],function($){
                 $(this).css('color','black').css('transition','0.7s');
                 $(this).find('div').css('opacity','1').css('transition','0.7s');
             })
-
-
+            //主页图片及文字描述的动画效果结束
+            //猜你喜欢部分数据加载
             $.ajax({
                 url: '../data/data3.json',
                 type: "GET",
                 success: function(res){
-                    var html1 = "";
-
                     for(var i = 0; i < res.length; i++){
-                        // html2+= `<li></li>`;
                         $("<li></li>").appendTo($(".like-list ul"));
-                        // $(".like-list ul").html(html2);
                         for(var j = 0; j < res[i].length; j++){
-
                             $(`<a href=""><div><img src="${res[i][j].img}" alt=""></div><p>${res[i][j].name}</p><span>${res[i][j].price}</span>
                                      </a>`).appendTo($(".like-list ul li").eq(i));
                         }
@@ -151,7 +160,7 @@ define(['jquery','jquery-cookie'],function($){
                     alert(msg);
                 }
             })
-
+            //猜你喜欢部分数据加载结束
 
 
 
